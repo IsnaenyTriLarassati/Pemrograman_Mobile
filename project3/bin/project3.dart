02bdd25d12eafba443e05c98c19e9c73387d210a
isnaenytrilarassati <isnaenytrilarassati>
2025-09-19 11:21:48 +0700

    Fixlist

--- project3/bin/project3.dart
+++ project3/bin/project3.dart
@@ -1,5 +1,16 @@
+import 'dart:io';
+
 import 'package:project3/project3.dart' as project3;
 
 void main(List<String> arguments) {
-  print('Hello world: ${project3.calculate()}!');
+
+  // Fixlist
+  var fl = List<int>.filled(4, 21); //21 = default value
+  fl[0] = 1;
+  fl[1] = 2;
+  fl[2] = 3;
+  fl[3] = 4;
+  // fl[4] = 5;
+
+  stdout.writeln(fl);
 }
