3ad6ff44c017ae36a6ebbfd517b30deb9c0be507
isnaenytrilarassati <isnaenytrilarassati>
2025-10-30 18:51:03 +0700

    Tugas_camera

--- project8/macos/Flutter/GeneratedPluginRegistrant.swift
+++ project8/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -4,7 +4,8 @@
 
 import FlutterMacOS
 import Foundation
-
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+    PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
